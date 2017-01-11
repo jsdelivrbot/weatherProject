69334c9f8909f1d35fdb0dbd1177d5f1a612a6bc
Scott Christensen <scottmc526@gmail.com>
2017-01-11 15:38:07 -0700

    axios install, ajax request setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 const API_KEY = 'f4821b5997aafd4659073f8fa75ce628';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+//actioncreaters have to return an action - object that has to have type property
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
+  })
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
