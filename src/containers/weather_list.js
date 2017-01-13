30434e75043732231eb8700e3d75c2caca02dc35
Scott Christensen <scottmc526@gmail.com>
2017-01-13 15:02:20 -0700

    kelvin to farenheit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,16 +4,18 @@ import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
-    console.log(cityData);
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    let temps = cityData.list.map(weather => weather.main.temp);
+    temps = temps.map(temp => {
+      return temp * (9/5) - 459.67;
+    })
     const pressure = cityData.list.map(pressure => pressure.main.pressure);
     const humidity = cityData.list.map(humidity => humidity.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color='orange' units='K' /></td>
+        <td><Chart data={temps} color='orange' units='F' /></td>
         <td><Chart data={pressure} color='blue' units='hPa' /></td>
         <td><Chart data={humidity} color='pink' units='%'/></td>
       </tr>
@@ -26,7 +28,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature (K)</th>
+            <th>Temperature (F)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
           </tr>
