c98ec01286c8732b1c311f406826aff1db15d832
Scott Christensen <scottmc526@gmail.com>
2017-01-13 12:52:22 -0700

    chart detail

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #Weather App Project with React and Redux
--This app allows the user to search for a particular location and view it's temperature, precip, and humidity predictions for the next 5 days.
+-This app allows the user to search for a particular location and view it's temperature, precip, and humidity predictions for the next 5 days. The weather data comes from the openweathermap.org API 
 
 #To Run :
 Clone and run `npm start`
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,6 +4,7 @@ import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    console.log(cityData);
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressure = cityData.list.map(pressure => pressure.main.pressure);
@@ -12,9 +13,9 @@ class WeatherList extends Component {
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color='orange' />
-        </td>
+        <td><Chart data={temps} color='orange' units='K' /></td>
+        <td><Chart data={pressure} color='blue' units='hPa' /></td>
+        <td><Chart data={humidity} color='pink' units='%'/></td>
       </tr>
     )
   }
@@ -25,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
